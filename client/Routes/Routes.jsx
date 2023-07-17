ca34f833ede6106b2687ab220d6e47790aedd909
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-07-17 14:55:42 +0600

    SignUp-form-route-component-create

--- client/Routes/Routes.jsx
+++ client/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../src/Layout/Main";
 import Home from "../src/Pages/Home/Home/Home";
+import Sign from "../src/Pages/SignUp/Sign";
 
 
 const routes =createBrowserRouter([
@@ -11,6 +12,10 @@ const routes =createBrowserRouter([
             {
                 path:"/",
                 element:<Home></Home>
+            },
+            {
+                path:'/sign',
+                element:<Sign></Sign>
             }
         ]
     }
--- client/src/Pages/Shared/Nav.jsx
+++ client/src/Pages/Shared/Nav.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import img from "../../assets/logo.png"
+import { Link } from 'react-router-dom';
 const Nav = () => {
     const [state, setState] = useState(false)
 
@@ -65,12 +66,12 @@ const Nav = () => {
                         <a href="javascript:void(0)" className="block text-gray-700 hover:text-gray-900">
                             Log in
                         </a>
-                        <a href="javascript:void(0)" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
-                            Sign in
+                        <Link to="/sign" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
+                            Sign Up
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                 <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
                             </svg>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
